9975a03f74f1a3cef50eebea0daf2deb5874147f
闫超 <ASOCS_yc@studentpartner.com>
2020-05-08 17:49:16 +0800

    添加了主键，变量及整数常量的判断

---
+++ lexical_analysis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace lexical_analysis
+{
+    class Program
+    {
+        public string[] word = { "", "", "" };
+
+        static void Main(string[] args)
+        {
+            string _0 = System.IO.File.ReadAllText("");
+            string[] _1 = _0.Split(' ');
+        }
+
+        string Scaner(string block)
+        {
+            string temp = $"({block},{word.ToList().IndexOf(block)})";
+            return temp;
+        }
+    }
+}
