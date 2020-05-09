00b5b73a3ff2a97de76d8932c82d9c84082e4bfe
闫超 <ASOCS_yc@studentpartner.com>
2020-05-09 11:34:47 +0800

    fix some bug，but Spilt can't spilt numble

--- lexical_analysis/Program.cs
+++ lexical_analysis/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace lexical_analysis
 {
@@ -11,14 +12,14 @@ namespace lexical_analysis
             "&&", "||" };
         private static readonly string[] keyWord = { "main", "int", "char", "char", "if", "else", "for", "while" };
         private static readonly string[] opWord = { "=", "+", "-", "*", "/", "(", ")", "[", "]", "{", "}", ",", ":", ";", ">", "<", ">=", "<=", "==", "!=", "&", "&&", "||" };
-        private static readonly string[] digit = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        private static readonly string[] digit = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","10" };
         private static readonly string[] letter = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
             "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D",
             "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
             "X", "Y", "Z" };
         static void Main(string[] args)
         {
-            string[] _0 = System.IO.File.ReadAllLines("test.txt");
+            string[] _0 = System.IO.File.ReadAllLines("C:/Users/13519/source/repos/lexical_analysis/lexical_analysis/test.txt");
             Program LA = new Program();
             int x = 0, y = 0;
             foreach (string line in _0)
@@ -27,19 +28,24 @@ namespace lexical_analysis
                 string[] _1 = line.Split(new char[3] { ' ', '\n', '\t' });
                 foreach (string item in _1)
                 {
+                    string formatLine = item;
                     foreach (var so in opWord)
                     {
                         //so is special opcode;
-                        string formatLine = "";
-                        if(item.IndexOf(so)!=-1)
+                        if(formatLine.IndexOf(so)!=-1)
                         {
-                            formatLine = item.Insert(item.IndexOf(so), " ");
+                            formatLine = formatLine.Insert(formatLine.IndexOf(so), " ");
+                            formatLine = formatLine.Insert(formatLine.IndexOf(so) + 1, " ");
                         }
-                        string[] formated = formatLine.Split(" ");
-                        foreach (string block in formated)
+                        
+                    }
+                    string[] formated = formatLine.Split(" ");
+                    foreach (string block in formated)
+                    {
+                        if(!block.Equals(""))
                         {
                             string result = LA.Scaner(block);
-                            if(result.Equals("error"))
+                            if (result.Equals("error"))
                             {
                                 y = line.IndexOf(block) + 1;
                                 LA.PrintError(block, x, y);
@@ -48,7 +54,7 @@ namespace lexical_analysis
                             {
                                 Console.WriteLine(result);
                             }
-                        }
+                        }  
                     }
                 }
             }
@@ -71,13 +77,13 @@ namespace lexical_analysis
             {
                 temp = $"({word.ToList().IndexOf("ID")},{block})";
             }
-            else if(IsNum(block))
-            {
-                temp = $"({word.ToList().IndexOf("NUM")},{block})";
-            }
             else if(IsOpWord(block))
             {
                 temp = $"({word.ToList().IndexOf(block)},{block})";
+            }
+            else if (IsNum(block))
+            {
+                temp = $"({word.ToList().IndexOf("NUM")},{block})";
             }
             else
             {
@@ -128,7 +134,8 @@ namespace lexical_analysis
         private bool IsNum(string block)
         {
             bool ret = true;
-            foreach (var item in block.Split(""))
+            string[] Num = block.Split("");
+            foreach (string item in Num)
             {
                 if (digit.ToList().IndexOf(item) == -1)
                 {
